804bcc76df63bbcf7936b36e62e91da008d58118
Luke Davies <lukedaviesweb@gmail.com>
2019-02-27 19:49:30 +0800

    front end

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://luke:luke@mernsocial-skkdp.mongodb.net/test',
+  mongoURI:
+    'mongodb://luke:luke@mernsocial-shard-00-00-skkdp.mongodb.net:27017,mernsocial-shard-00-01-skkdp.mongodb.net:27017,mernsocial-shard-00-02-skkdp.mongodb.net:27017/test?ssl=true&replicaSet=MERNSocial-shard-0&authSource=admin&retryWrites=true',
 
   secretOrKey: 'secret'
 };
